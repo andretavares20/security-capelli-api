5461a13ff3babc4b4d1a9e468efaa0b015b12e58
Andre Tavares <andretavares16@gmail.com>
2023-12-07 21:01:46 -0300

    mudando endpoints do cliente para get

--- src/main/java/com/andretavares/testesecurity/controllers/TamanhoController.java
+++ src/main/java/com/andretavares/testesecurity/controllers/TamanhoController.java
@@ -4,12 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.andretavares.testesecurity.dto.TamanhoDto;
-import com.andretavares.testesecurity.entities.Tamanho;
 import com.andretavares.testesecurity.services.TamanhoService;
 
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,7 @@ public class TamanhoController {
     @Autowired
     private TamanhoService tamanhoService;
 
-    @PostMapping()
+    @GetMapping()
     public ResponseEntity<List<TamanhoDto>> listTamanho() {
 
         return ResponseEntity.ok().body(tamanhoService.getListTamanho());
